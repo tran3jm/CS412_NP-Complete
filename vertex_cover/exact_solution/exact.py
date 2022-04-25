b2440c024a8b683c0eceb915b58a27f1aa0304c3
Felix <taylo4ag@dukes.jmu.edu>
2022-04-25 13:51:06 -0400

    exact

--- vertex_cover/exact_solution/exact.py
+++ vertex_cover/exact_solution/exact.py
@@ -1,12 +1,27 @@
+import itertools
 def main():
     n = int(input())
     g = {}
+    vSet = []
     for _ in range(n):
         edges = input().split(" ")
         node = edges[0]
+        vSet.append(node)
         g[node] = set()
         for x in range(1, len(edges)):
             g[node].add(edges[x])
+    def exact():
+        for x in range(1, len(vSet) + 1):
+            combo = list(itertools.combinations(vSet, x))
+            for item in combo:
+                l = []
+                for entry in list(item):
+                    for v in g[entry]:
+                        l.append(v)
+                    l.append(entry)
+                    if len(set(l)) == len(vSet): return list(item)
+        return list(vSet)
+    print(*exact())
 
 if __name__ == "__main__":
     main()
