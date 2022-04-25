9f9c44dc38eef666a5523ab4e8c48d82bae2c110
Felix <taylo4ag@dukes.jmu.edu>
2022-04-25 14:53:49 -0400

    comments

--- vertex_cover/exact_solution/exact.py
+++ vertex_cover/exact_solution/exact.py
@@ -1,8 +1,9 @@
 import itertools
 def main():
-    n = int(input())
-    g = {}
-    vSet = []
+    n = int(input()) # number of vertices in graph
+    g = {} # graph
+    vSet = [] # set of all vertices in graph
+    # setting up the graph and vertex set
     for _ in range(n):
         edges = input().split(" ")
         node = edges[0]
@@ -11,16 +12,25 @@ def main():
         for x in range(1, len(edges)):
             g[node].add(edges[x])
     def exact():
-        for x in range(1, len(vSet) + 1):
-            combo = list(itertools.combinations(vSet, x))
-            for item in combo:
-                l = set()
+        # for every possible number of vertices
+        for x in range(1, len(vSet)):
+            # generate combinations
+            combos = list(itertools.combinations(vSet, x))
+            # for every combination
+            for item in combos:
+                l = []
+                # get the vertices connected to each resulting vertex
+                # and add them to l
                 for entry in list(item):
                     for v in g[entry]:
-                        l.add(v)
-                    l.add(entry)
-                if len(l) == len(vSet): return list(item)
+                        l.append(v)
+                    l.append(entry)
+                    # if all vertices are included in l
+                    # return this specific combination
+                    if len(set(l)) == len(vSet): return list(item)
+        return list(vSet)
     print(*exact())
 
 if __name__ == "__main__":
     main()
+ 
