58485677834a3aaf07b5c9ea4ce6a02a3fab4de0
Felix <taylo4ag@dukes.jmu.edu>
2022-04-26 12:16:08 -0400

    updated

--- vertex_cover/exact_solution/exact.py
+++ vertex_cover/exact_solution/exact.py
@@ -3,33 +3,38 @@ def main():
     n = int(input()) # number of vertices in graph
     g = {} # graph
     vSet = [] # set of all vertices in graph
+    edgeC = 0
     # setting up the graph and vertex set
     for _ in range(n):
         edges = input().split(" ")
-        node = edges[0]
+        node = int(edges[0])
         vSet.append(node)
         g[node] = set()
         for x in range(1, len(edges)):
-            g[node].add(edges[x])
+            g[node].add(int(edges[x]))
+            edgeC += 1
+    edgeC /= 2
     def exact():
+        if edgeC == 0: return []
         # for every possible number of vertices
         for x in range(1, len(vSet)):
             # generate combinations
             combos = list(itertools.combinations(vSet, x))
             # for every combination
             for combo in combos:
-                l = set()
-                # get the vertices connected to each generated vertex
-                # and add them to l
+                edgeSet = set()
+                # for every entry in combination
                 for entry in list(combo):
+                    # for every vertex in the entry
                     for v in g[entry]:
-                        l.add(v)
-                    l.add(entry)
-                    # if all vertices are included in l
-                    # return this specific combination
-                    if len(l) == len(vSet): return list(combo)
-        return list(vSet)
-    print(*exact())
+                        # add its edges to edgeSet
+                        if (v, entry) not in edgeSet: edgeSet.add((entry, v))
+                    # return when length of edgeSet = # of edges
+                    if len(edgeSet) == edgeC: return list(combo)
+        return []
+    res = exact()
+    if len(res) == 0: print("There are no edges.")
+    else: print(*res)
 
 if __name__ == "__main__":
     main()
