c4d7151cb915c7b82e51d3d526d4cd9bbd25e689
Justin <59967010+JayFourJustin@users.noreply.github.com>
2022-04-27 21:04:07 -0400

    Update cs412TestGen.py
    fixed N

--- cs412TestGen.py
+++ cs412TestGen.py
@@ -2,31 +2,33 @@
 import random
 
 def generate():
+    counter = 0
     n = random.randrange(1, 20)
     d = {}
-    for i in range(n - 1):
+    for i in range(n):
         d[i] = set()
 
-    for i in range(n - 1):
+    for i in range(n):
 
-        nodes = random.randrange(0, n - 1)
+        nodes = random.randrange(0, n)
         
         for j in range(nodes):
 
-            rand = random.randrange(0, n - 1)
+            rand = random.randrange(0, n)
 
             if j != rand and rand != i and i != j:
+                counter += 1
                 d[i].add(rand)
                 d[rand].add(i)
 
-    print(n - 1)
+    print(n)
     for i in d:
         print(i,end=" ")
         for j in d[i]:
             print(j,end=" ")
         
         print()
-
+    print(counter)
 
 def main():
     generate()
